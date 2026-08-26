335f77e2db03dd9f385a8d8d70afa70ec38a2e42
King Singh <khaiwilsan676@gmail.com>
2026-08-26 14:05:39 +0530

    Disable trailing slash redirect for API

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-}
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
+};
 
-export default nextConfig
+export default nextConfig;
