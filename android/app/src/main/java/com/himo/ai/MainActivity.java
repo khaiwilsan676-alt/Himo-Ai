652bca120fe485e2c2dc2006466c630e120c45b0
King Singh <khaiwilsan676@gmail.com>
2026-08-29 01:16:59 +0530

    Fix Android fullscreen and update dependencies

--- android/app/src/main/java/com/himo/ai/MainActivity.java
+++ android/app/src/main/java/com/himo/ai/MainActivity.java
@@ -1,8 +1,8 @@
 package com.himo.ai;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+
+import androidx.core.view.WindowCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -11,13 +11,14 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        var controller = WindowCompat.getInsetsController(
+                getWindow(),
+                getWindow().getDecorView()
         );
+
+        controller.setAppearanceLightStatusBars(false);
+        controller.setAppearanceLightNavigationBars(false);
     }
 }
